a1b1df197be8f67c1d68507b0e59e09ea3c717e8
Trankvila <thedamianpl@gmail.com>
2019-08-22 18:21:00 +0200

    Add stats site

---
+++ stats.php
@@ -0,0 +1,38 @@
+<?php 
+session_start();
+?>
+<!DOCTYPE html>
+<html class="no-js">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Mango - Twój dziennik żywieniowy</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/style.css">
+        <link href="//fonts.googleapis.com/css?family=Ubuntu&display=swap" rel="stylesheet"> 
+        <script src="https://kit.fontawesome.com/a706ae0628.js"></script>
+       
+    </head>
+    <body>
+
+    <nav class="navigation"> 
+            <div class="logo">MANGO</div>
+    <ul class="menu"> 
+        <li>kontakt</li>
+        <li>o nas</li>
+    </ul>
+    </nav>
+    <section class="userInterfaceBox">
+
+    </section>  
+
+<?php 
+if(!isset($_SESSION['emailUsers'])){
+    header("Location: ../mango/index.php");
+    die();
+}
+?>
+    
+    </body>
+</html>
