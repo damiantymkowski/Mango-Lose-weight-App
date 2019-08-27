53c32c4ddfaa1d8f999e1ebeed5c8f3ad4b056a5
Trankvila <thedamianpl@gmail.com>
2019-08-27 17:03:22 +0200

    Fix json database

--- includes/delete_stats.inc.php
+++ includes/delete_stats.inc.php
@@ -3,5 +3,11 @@
     session_start();
     require 'dbh.inc.php';
 
-    mysqli_query($conn, "DELETE FROM usersbmi WHERE iduser ='".$_SESSION['emailUsers']."'");
+    $deleteBMI = mysqli_query($conn, "DELETE FROM usersbmi WHERE iduser ='".$_SESSION['emailUsers']."'");
 
+    if($deleteBMI!==FALSE)
+    {
+        echo(403);
+    }else{
+        echo(401);
+    }
