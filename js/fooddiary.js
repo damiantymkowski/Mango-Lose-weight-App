11ccf767f1cc02649f4a485f4ae3165af4e972bb
Trankvila <thedamianpl@gmail.com>
2019-09-06 21:45:31 +0200

    Update food diary system

--- js/fooddiary.js
+++ js/fooddiary.js
@@ -24,12 +24,25 @@ const postDiaryData = () =>{
     });
 }
 
+let formatDate = (dateString) => {
+    var date = new Date(dateString),
+      dd = date.getDate(),
+      mm = date.getMonth() + 1,
+      yyyy = date.getFullYear();
+      hh = date.getHours();
+      min = date.getMinutes();
+    mm = mm < 10 ? '0' + mm : mm;
+    return dd + '-' + mm +'-' + yyyy + ' ' + hh + ':' + min;
+  }
+
 fetch("includes/foodlist.inc.php")
 .then(res=>res.json())
 .then(res=>{
       for(let i=0; i<res.length; i++){
-        template.querySelector('.userBMI').textContent = Math.round(res[i]["0"]*100)/100;
-        template.querySelector('.measurementDate').textContent = formatDate(res[i]["1"]);
+        template.querySelector('.foodDate').textContent = formatDate(res[i]["0"]);
+        template.querySelector('.foodCalories').textContent = res[i]["1"];
+        template.querySelector('.foodName').textContent = res[i]["2"];
+
         let tbody = document.querySelector("tbody");
         let clone = document.importNode(template, true);
         let td = clone.querySelectorAll("td");
